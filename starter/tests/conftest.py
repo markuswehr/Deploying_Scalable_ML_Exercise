bbb25431be34268efabe0efa80906b514f331b59
Markus Wehr <wehr.markus@web.de>
2023-01-18 10:10:18 +0100

    updated path to cleaned csv in conftest.py

--- starter/tests/conftest.py
+++ starter/tests/conftest.py
@@ -8,6 +8,9 @@ from sklearn.ensemble import RandomForestClassifier
 from starter.ml.data import process_data
 
 
+root_path = os.path.dirname(os.path.abspath(__file__))
+
+
 @pytest.fixture(scope="session")
 def data():
     df = pd.DataFrame(np.random.randint(0, 400, size=(200, 4), ), columns=["A", "B", "C", "D"])
@@ -29,7 +32,7 @@ def model(data):
 @pytest.fixture(scope='session')
 def test_data(data):
     # Add code to load in the data.
-    data = pd.read_csv("../../data/census_cleaned.csv")
+    data = pd.read_csv(os.path.join(root_path, "starter/data/census_cleaned.csv"))
     cat_features = [
         "workclass",
         "education",
